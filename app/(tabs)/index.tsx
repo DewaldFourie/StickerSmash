1ab048ee34c0746fda9fdd2a160513837b567330
Dewald <dewaldfourie0808@gmail.com>
2025-03-05 13:51:52 +0200

    added ImageViewer and Button components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import { View, StyleSheet } from "react-native";
+
+import ImageViewer from "@/components/ImageViewer";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href='/about' style={styles.button}>Go to About Screen</Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage}/>
+      </View>
     </View>
   );
 }
@@ -14,15 +18,9 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#25292e",
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    color: "#fff",
-    textDecorationLine: "underline",
-  }
 });
